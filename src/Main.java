fde438a7f239d42101e268313391aafc32568e50
Sergey Kashirin <kashirin-1984@mail.ru>
2023-03-06 18:17:57 +0300

    if-while

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello Sergey!");
+        Scanner q=new Scanner(System.in);
+        int w= q.nextInt();
+        if (w>99&&w<1000||w<-99&&w>-1000){
+            System.out.println("yes");
+        }
+        else {
+            System.out.println("no");
+        }
     }
-
-
 }
